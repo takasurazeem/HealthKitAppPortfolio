9723128bf9f257b372781ed9702ee8857cc2029f
Takasur Azeem <takasurazeem@gmail.com>
2024-08-04 12:18:33 +0500

    Initial Commit

---
+++ HealthKitAppPortfolio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthKitAppPortfolio
+//
+//  Created by Takasur Azeem on 8/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthKitAppPortfolio/HealthKitAppPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitAppPortfolioApp.swift
+//  HealthKitAppPortfolio
+//
+//  Created by Takasur Azeem on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitAppPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
